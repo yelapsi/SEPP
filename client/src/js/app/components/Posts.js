b3378ba3d62357792b52054e9a755a7379161259
yeeh1616 <50901300+yeeh1616@users.noreply.github.com>
2022-03-22 15:41:43 +0100

    Update Posts.js

--- client/src/js/app/components/Posts.js
+++ client/src/js/app/components/Posts.js
@@ -1,36 +1,71 @@
 import React from 'react'
+
 import ClassClick from './ClassClick';
+
 import '../App.css';
 
+
+
 const Posts = ({posts, loading, config}) => {
+
     if (loading) {
+
         return <h2>Loading..</h2>;
+
     }
+
+
 
     console.log("posts.num: " + posts.length);
 
+
+
     return (
+
         <table>
-            <tr>
-                <th>#</th>
-                <th>User Id</th>
-                <th>Type</th>
-                <th>Tracking Type</th>
-                <th>Details</th>
-                <th>Timestamp</th>
+
+            <tr class="logui_tr">
+
+                <th class="logui_th">#</th>
+
+                <th class="logui_th">User_Id</th>
+
+                <th class="logui_th">Type</th>
+
+                <th class="logui_th">Tracking_Type</th>
+
+                <th class="logui_th">Details</th>
+
+                <th class="logui_th">Timestamp</th>
+
             </tr>
+
             {posts.map(post => (
+
                 <tr>
-                    <td>{post['id']}</td>
-                    <td>{post['user_id']}</td>
-                    <td>{post['type']}</td>
-                    <td>{post['tracking_type']}</td>
-                    <td>{JSON.stringify(post['details'])}</td>
-                    <td>{post['time_stamp']}</td>
+
+                    <td class="logui_td logui_td_id">{post['id']}</td>
+
+                    <td class="logui_td">{post['user_id']}</td>
+
+                    <td class="logui_td">{post['type']}</td>
+
+                    <td class="logui_td">{post['tracking_type']}</td>
+
+                    <td class="logui_td_sp">{JSON.stringify(post['details'])}</td>
+
+                    <td class="logui_td_sp">{post['time_stamp']}</td>
+
                 </tr>
+
             ))}
+
         </table>
+
     )
+
 }
+
+
 
 export default Posts
